7ddab4488f524cea10b73607e36ee60b984e1ae8
Languzda <langusda@gmail.com>
2023-04-08 15:54:56 +0200

    add redux scope

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,19 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
+import { Provider } from "react-redux";
+
 import App from "./App";
 import ShopContextProvider from "./store/shop-context";
+import store from "./store";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
-  <ShopContextProvider>
-    <App />
-  </ShopContextProvider>
+  <Provider store={store}>
+    <ShopContextProvider>
+      <App />
+    </ShopContextProvider>
+  </Provider>
 );
